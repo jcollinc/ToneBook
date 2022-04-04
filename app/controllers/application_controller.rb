0d53a68f292e22c5d119e279f118bf4d8f05b5c1
Jon Chinje <j.chinje@gmail.com>
2022-04-04 13:55:30 -0400

    basic front-end setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+  rescue_from ActiveRecord::RecordInvalid, with: :render_invalid
+
+  private 
+
+  def render_invalid(e)
+    render json: {errors: e.record.errors.full_messages}, status: :unprocessable_entity
+  end 
+
+  def render_not_found()
+      render json: {error: "Not Found"}, status: :not_found
+  end 
 
 end
